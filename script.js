004c5fa88c32e43df002b14ed35c9fd7d85d3a28
powermate <218421152+powermate25@users.noreply.github.com>
2025-10-02 17:16:43 +0000

    Sandbox: RPS game UI design done. minor bugs to be fixed. Checkpoint save.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 // Script
 console.log("WELCOME TO ROCK PAPER SCISSORS GAME");
 // alert("WELCOME TO ROCK PAPER SCISSORS GAME!");
@@ -14,9 +15,9 @@ console.log("WELCOME TO ROCK PAPER SCISSORS GAME");
 
 function getComputerChoice(){
    let result = Math.random();
-   if(result <= 0.3){ return "Rock" }
-   else if (result >= 0.6){return "Paper"}
-   else {return "Scissors"}; 
+   if(result <= 0.3){ return "rock" }
+   else if (result >= 0.6){return "paper"}
+   else {return "scissors"}; 
 }
     
   
@@ -26,23 +27,41 @@ let computerScore = 0;
 let win = "You win!"
 let lose = "CPU wins"
 let tie = "Tie. Let's try again!" 
+
     
 function playRound( humanChoice = getHumanChoice(), computerChoice = getComputerChoice() ){
+
+    const rockImgCpu = document.createElement("img")
+       rockImgCpu.src = "./images/cpu/cpu-hand-back-fist-solid-full.svg";
+       rockImgCpu.height = 100;
+       
+
+     const paperImgCpu = document.createElement("img")
+       paperImgCpu.src = "./images/cpu/cpu-hand-solid-full.svg";
+       paperImgCpu.height = 100;
+
+     const scissorsImgCpu = document.createElement("img")
+       scissorsImgCpu.src = "./images/cpu/cpu-hand-scissors-solid-full.svg";
+       scissorsImgCpu.height = 100;
+
+       if(computerChoice === "rock"){ document.querySelector("#cpuImage").replaceChildren(rockImgCpu) }
+       else if(computerChoice === "paper"){ document.querySelector("#cpuImage").replaceChildren(paperImgCpu) }
+       else if(computerChoice === "scissors"){ document.querySelector("#cpuImage").replaceChildren(scissorsImgCpu) }  
     
  
    
 
-    if (humanChoice == "rock" && computerChoice == "Rock") return tie + " " + humanChoice.toUpperCase() + " vs " + computerChoice.toUpperCase()
-    else if (humanChoice == "paper" && computerChoice == "Rock") return win + " | " + humanChoice.toUpperCase() + " wraps " + computerChoice.toUpperCase() + " | Your score: " + (++humanScore)
-    else if (humanChoice == "scissors" && computerChoice == "Rock") return lose + " ( " + computerChoice.toUpperCase() + " crushes " + humanChoice.toUpperCase() + " | CPU score: " + (++computerScore)
+    if (humanChoice == "rock" && computerChoice == "rock") return tie + " " + humanChoice.toUpperCase() + " vs " + computerChoice.toUpperCase()
+    else if (humanChoice == "paper" && computerChoice == "rock") return win + " | " + humanChoice.toUpperCase() + " wraps " + computerChoice.toUpperCase() + " | Your score: " + (++humanScore)
+    else if (humanChoice == "scissors" && computerChoice == "rock") return lose + " ( " + computerChoice.toUpperCase() + " crushes " + humanChoice.toUpperCase() + " | CPU score: " + (++computerScore)
 
-    else if (humanChoice == "rock" && computerChoice == "Paper") return lose + " | " + computerChoice.toUpperCase() + " wraps " + humanChoice.toUpperCase() + " | CPU score: " + (++computerScore)
-    else if (humanChoice == "paper" && computerChoice == "Paper") return tie + " " + humanChoice.toUpperCase() + " vs " + computerChoice.toUpperCase()
-    else if (humanChoice == "scissors" && computerChoice == "Paper") return win + " | " + humanChoice.toUpperCase() + " cut " + computerChoice.toUpperCase() + " | Your score: " + (++humanScore)
+    else if (humanChoice == "rock" && computerChoice == "paper") return lose + " | " + computerChoice.toUpperCase() + " wraps " + humanChoice.toUpperCase() + " | CPU score: " + (++computerScore)
+    else if (humanChoice == "paper" && computerChoice == "paper") return tie + " " + humanChoice.toUpperCase() + " vs " + computerChoice.toUpperCase()
+    else if (humanChoice == "scissors" && computerChoice == "paper") return win + " | " + humanChoice.toUpperCase() + " cut " + computerChoice.toUpperCase() + " | Your score: " + (++humanScore)
 
-    else if (humanChoice == "rock" && computerChoice == "Scissors") return win + " | " + humanChoice.toUpperCase() + " crushes " + computerChoice.toUpperCase() + " | Your score: " + (++humanScore)
-    else if (humanChoice == "paper" && computerChoice == "Scissors") return lose + " | " + computerChoice.toUpperCase() + " cut " + humanChoice.toUpperCase() + " | CPU score: " + (++computerScore)
-    else if (humanChoice == "scissors" && computerChoice == "Scissors") return tie + " " + humanChoice.toUpperCase() + " vs " + computerChoice.toUpperCase()
+    else if (humanChoice == "rock" && computerChoice == "scissors") return win + " | " + humanChoice.toUpperCase() + " crushes " + computerChoice.toUpperCase() + " | Your score: " + (++humanScore)
+    else if (humanChoice == "paper" && computerChoice == "scissors") return lose + " | " + computerChoice.toUpperCase() + " cut " + humanChoice.toUpperCase() + " | CPU score: " + (++computerScore)
+    else if (humanChoice == "scissors" && computerChoice == "scissors") return tie + " " + humanChoice.toUpperCase() + " vs " + computerChoice.toUpperCase()
 };        
     
 
@@ -80,19 +99,33 @@ function playGame(){
     if(humanScore === 5){
         result.textContent = winner
         score.textContent = ( "Your score: " + humanScore + " | " + "CPU score: " + computerScore);
-        score.append(newDiv.textContent="Congratulation! You're the final winner.")
+        score.append(newDiv.textContent="\n Congratulation! You're the final winner. 🏆🎉")
         // alert("Congratulation! You're the final winner.")
+       const playAgain = document.createElement("div")
+       playAgain.classList.add("playAgain")
+       playAgain.textContent = "PLAY AGAIN?"
+       resetContainer.appendChild(playAgain)
+
         resetContainer.append(resetButton)
+        resetContainer.appendChild(cancelButton)
         resetButton.addEventListener("click", () => {window.location.reload()})
+        cancelButton.addEventListener("click",() => {alert("GAME OVER!")})
       // confirm("GAME OVER. Play again?", window.location.reload())
     }
     else if (computerScore === 5){
         result.textContent = winner
         score.textContent = ( "Your score: " + humanScore + " | " + "CPU score: " + computerScore);
-        score.append(newDiv.textContent="CPU won!")
+        score.append(newDiv.textContent="\n \n CPU WON! 😎") 
        // alert("CPU won!")
+       const playAgain = document.createElement("div")
+       playAgain.classList.add("playAgain")
+       playAgain.textContent = "PLAY AGAIN?"
+       resetContainer.appendChild(playAgain)
+
        resetContainer.append(resetButton)
+       resetContainer.appendChild(cancelButton)
        resetButton.addEventListener("click", () => {window.location.reload()})
+       cancelButton.addEventListener("click",() => {alert("GAME OVER!")})
        
 
       // confirm("GAME OVER. Play again?", window.location.reload())
@@ -111,7 +144,8 @@ const btnRock = document.querySelector("#rock");
 btnRock.addEventListener(
     "click", () => {
         userInput = btnRock.id
-        playGame()
+        document.querySelector("#userImage").replaceChildren(rockImg)
+        playGame() 
        // confirm("GAME OVER. Play again?", window.location.reload())
     }  
 ) 
@@ -120,6 +154,7 @@ const btnPaper = document.querySelector("#paper");
 btnPaper.addEventListener(
     "click", () => {
         userInput = btnPaper.id
+        document.querySelector("#userImage").replaceChildren(paperImg)
         playGame()
         // confirm("GAME OVER. Play again?", window.location.reload())
     }  
@@ -129,6 +164,7 @@ const btnScissors = document.querySelector("#scissors");
 btnScissors.addEventListener(
     "click", () => {
         userInput = btnScissors.id
+        document.querySelector("#userImage").replaceChildren(scissorsImg)
         playGame()
        // confirm("GAME OVER. Play again?", window.location.reload())
     }  
@@ -136,6 +172,28 @@ btnScissors.addEventListener(
 
        const resetButton = document.createElement("button")
        resetButton.classList.add("resetbutton")
-       resetButton.textContent = "PLAY AGAIN?"
+       resetButton.textContent = "YES!"
        resetContainer = document.querySelector("#reset")
+
+       const cancelButton = document.createElement("button")
+       cancelButton.classList.add("cancelbutton")
+       cancelButton.textContent = "NO!"
+       resetContainer = document.querySelector("#reset")
+
+       const rockImg = document.createElement("img")
+       rockImg.src = "./images/user/hand-back-fist-solid-full.svg";
+       rockImg.height = 100;
+
+       const paperImg = document.createElement("img")
+       paperImg.src = "./images/user/hand-solid-full.svg";
+       paperImg.height = 100;
+
+       const scissorsImg = document.createElement("img")
+       scissorsImg.src = "./images/user/hand-scissors-solid-full.svg";
+       scissorsImg.height = 100;
+
+       
+
+
+       
        
